f8b4dea87b95652db8eefeb4867b8b5d612b7e50
Imran <imran@wsegames.com>
2022-04-16 22:44:29 +0530

    navbar and banner addedd

--- src/components/header.js
+++ src/components/header.js
@@ -5,24 +5,34 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            colorChange: false
         }
     }
 
-    componentDidMount() {
-
+    changeNavbarColor = () =>{
+        if(window.scrollY >= 80){
+            this.setState({
+                colorChange : true
+            })
+        }else{
+            this.setState({
+                colorChange: false
+            })
+        }
     }
 
+
     render() {
+        window.addEventListener('scroll', this.changeNavbarColor);
         return (
-            <nav className="navbar navbar-expand-lg header">
+            <nav className="navbar fixed-top navbar-expand-lg header" style={{backgroundColor:this.state.colorChange ? 'black' : 'transparent'}}>
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                         <section className="header-main">
-                            <div><a className="navbar-brand" href="/">Hidden brand</a></div>
+                            <div><a className="navbar-brand" href="/"><img src="https://eu-images.contentstack.com/v3/assets/bltaec35894448c7261/blt019398d0069c7adb/61f75013935106184ab7add1/pepperstone-logo-inverse-rgb.svg/?"/></a></div>
                             <div>
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                     <li className="nav-item">
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import Header from '../components/header';
-
+// import OwlCarousel from 'react-owl-carousel';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel/dist/assets/owl.theme.default.css';
 class Home extends Component {
     render() {
         return (
             <div>
-                <Header/>
+                <Header />
+                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src="https://ik.imagekit.io/xnqvm1iiw/pexels-energepiccom-159888_DPX1CEFn6.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1650127054739" className="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block custom-caption">
+                                <h1 className="banner-text">In a world of probabilities, trade the possibilities</h1>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://ik.imagekit.io/xnqvm1iiw/pexels-pixabay-210607_8V68YedK7.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1650127160893" className="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block custom-caption">
+                                <h1 className="banner-text">When markets are volatile, opportunity can be abundant. </h1>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://ik.imagekit.io/xnqvm1iiw/pexels-anna-nekrashevich-6801874_zhVNNSA_d.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1650127217120" className="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block custom-caption">
+                                <h1 className="banner-text">You need a Broker you can trust. Make your move with Pepperstone.</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
